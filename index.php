ea52ea02e94c1a681ee2e1d9583eeadbf9e8cb01
Narut Poovorakit <net.narut.p@gmail.com>
2018-03-15 18:38:40 +0700

    Init project

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <div class="container">
+        <div>
+            <div style="display: inline" id="title1">Previous font type: </div>
+            <div style="display: inline" id="previousFontType">Previous</div>
+        </div>
+        <div>
+            <div style="display: inline" id="title1">Previous font size: </div>
+            <div style="display: inline" id="previousFontType">12</div>
+        </div>
+        <div>
+            <div style="display: inline" id="title1">Current font type: </div>
+            <div style="display: inline" id="previousFontType">Current</div>
+        </div>
+        <div>
+            <div style="display: inline" id="title1">Current font type: </div>
+            <div style="display: inline" id="previousFontType">15</div>
+        </div>
+        <div>
+            <div style="display: inline" id="title1">Font type: </div>
+            <input id="fontType" type="text" />
+        </div>
+        <div>
+            <div style="display: inline" id="title1">Font size: </div>
+            <input id="fontType" type="text" />
+        </div>
+    </div>
+    <?php
+    echo "My first PHP script!";
+    ?>
+</body>
+</html>
